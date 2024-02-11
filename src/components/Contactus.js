4ab6a27590e6b80713904d29f5f4ab5ff027be82
mrafeeqvr <muhammedrafeeq805@gmail.com>
2024-02-11 19:48:26 +0530

    unit test in done by using jest and react testing library

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -2,7 +2,7 @@ const Contactus = () => {
     return (
         <div>
             <h1>Contact US PAGE</h1>
-            <h2>Our company contact us page </h2>
+            <label>Our company contact us page</label>
         </div>
     )
 }
---
+++ src/components/__test__/Contactus.test.js
@@ -0,0 +1,14 @@
+import { render,screen } from "@testing-library/react"
+import Contactus from "../Contactus"
+import "@testing-library/jest-dom"
+
+test('testing contact up page heading',() =>{
+    render(<Contactus/>)
+    const heading = screen.getByRole('heading')
+    expect(heading).toBeInTheDocument()
+})
+test('testing contact up page label',() =>{
+    render(<Contactus/>)
+    const label = screen.getByText('Our company contact us page')
+    expect(label).toBeInTheDocument()
+})
