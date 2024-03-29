06bae424d840b52538da004fd59edfcb7a97fb3c
mrafeeqvr <muhammedrafeeq805@gmail.com>
2024-03-29 15:29:25 +0530

    bug fixed

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 import React,{lazy,Suspense,useEffect,useState} from 'react'
 import ReactDOM from 'react-dom/client';
-import Header from './components/Header';
+import Header from './src/components/Header';
 import { createBrowserRouter,RouterProvider,Outlet } from 'react-router-dom';
-import CardsContainer from './components/CardContainer';
+import CardsContainer from './src/components/CardContainer';
 //import Aboutus from './components/Aboutus';
-import Contactus from './components/Contactus';
-import Error404 from './components/Error404';
-import RestaurentDetails from './components/CountryDetails';
-import Shimmer from './components/Shimmer';
-import UserContext from './utils/UserContext';
+import Contactus from './src/components/Contactus';
+import Error404 from './src/components/Error404';
+import RestaurentDetails from './src/components/CountryDetails';
+import Shimmer from './src/components/Shimmer';
+import UserContext from './src/utils/UserContext';
 import { Provider } from 'react-redux';
-import appStore from './utils/appStore';
-import Favorites from './components/Favorites';
+import appStore from './src/utils/appStore';
+import Favorites from './src/components/Favorites';
 
 
-const Aboutus = lazy(()=>import('./components/Aboutus'))
+const Aboutus = lazy(()=>import('./src/components/Aboutus'))
 const AppLayout = () =>{
     const [name,setName] = useState('')
     useEffect(()=>{
